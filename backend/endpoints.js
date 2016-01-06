505405d62961d97125d176dbcf61f4870501aa0d
cristianames <cristianames.92@gmail.com>
2016-01-06 14:54:28 -0300

    d

--- backend/endpoints.js
+++ backend/endpoints.js
@@ -2,23 +2,28 @@ var Etiqueta = require('./modelo/Etiqueta');
 
 exports.inyectar = function(app) {
 
-    app.get('/api/etiqueta', function(req, res) {
-        res.json({items: [
-            new Etiqueta('tag1'),
-            new Etiqueta('tag2'),
-            new Etiqueta('tag3'),
-            new Etiqueta('tag4'),
-            new Etiqueta('tag5'),
-            new Etiqueta('tag6')
-        ]});
+    app.get('/api/etiqueta', function (req, res) {
+        res.json({
+            items: [
+                new Etiqueta('tag1'),
+                new Etiqueta('tag2'),
+                new Etiqueta('tag3'),
+                new Etiqueta('tag4'),
+                new Etiqueta('tag5'),
+                new Etiqueta('tag6')
+            ]
+        });
     });
 
-    app.get('/api/publicacion', function(req,res){
-        res.json({items:[
-            new Etiqueta('Publicacion tramposa 1'),
-            new Etiqueta('Publicacion tramposa 2')
-        ]});
+    app.get('/api/publicacion', function (req, res) {
+        res.json({
+            items: [
+                new Etiqueta('Publicacion tramposa 1'),
+                new Etiqueta('Publicacion tramposa 2')
+            ]
+        });
     });
+}
 
     //app.post('/api/publicacion',var publicacion,function(req,res){
     //
