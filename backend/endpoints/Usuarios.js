b9e07bd503ee05cba307dde6cf5d8768016ab5d4
bruno <brunomendez88@hotmail.com>
2016-01-19 14:35:18 -0300

    Funcionando el registrarse. Firebase crea el usuario y devuelve un token sin reglan todavia. el user id se persiste al mismo tiempo en el la base de dato de firebase. O sea firebase tien un registro de los usuarios y paralelamente nosotros lo guardamos en nuestra base con el resto de los datos.

--- backend/endpoints/Usuarios.js
+++ backend/endpoints/Usuarios.js
@@ -1,6 +1,7 @@
 var Usuario = require('../modelo/Usuario');
 var Firebase = require("firebase");
 var usuarios = new Firebase('https://odingrid.firebaseio.com/usuarios');
+var firebase = new Firebase('https://odingrid.firebaseio.com');
 
 exports.inyectar = function(app) {
 
@@ -12,11 +13,21 @@ exports.inyectar = function(app) {
      });
      });
      */
-    app.post('/api/vs1.0/uuario', function (req, res) {
-        //console.log(req.body);
-        var obj = usuarios.push().set(req.body);  //Estoy pusheando un equipo en firebase con id autogenerado en lista de equipo
+
+    var crearUsuario = function(user){
+        console.log("entre a crear usuario");
+        alert(user.contrasenia);
+
+    }
+
+    app.post('/api/v1.0/usuario', function (req, res) {
+        console.log(req.body);
+        var obj = usuarios.push().set(req.body);  //Estoy pusheando un usuario en firebase con id autogenerado en lista de equipo
         res.send("Exito!");
         alert("se creo usuario");
+
     });
+
+
 
 }
