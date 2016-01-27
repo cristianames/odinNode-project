72ab627a9f409c26c1f88248901524c59815dd1c
marrubgui <marianoguido@yahoo.com.ar>
2016-01-27 01:18:54 -0300

    Merge branches 'mariano' and 'master' of https://github.com/cristianames/odinNodeProject into mariano
    # Conflicts:
    #	public/javascripts/controllers/mainController.js
    #	public/javascripts/controllers/publicaciones/nuevaPublicacionController.js

--- public/javascripts/controllers/publicaciones/nuevaPublicacionController.js
+++ public/javascripts/controllers/publicaciones/nuevaPublicacionController.js
@@ -1,13 +1,22 @@
-app.controller("nuevaPublicacionController", ['$scope', '$location', 'PublicacionesFactory', '$rootScope', function(scope, location, Publicaciones, $rootScope){
-    scope.publicacionDescripcion = "Inserte breve descripción.";
-    scope.publicacionContenido = "Inserte contenido.";
+app.controller("nuevaPublicacionController", ['$scope', '$location', 'PublicacionesFactory','EtiquetasFactory', '$rootScope', function(scope, location, Publicaciones,Etiquetas, $rootScope){
     scope.publicacionCheckbox = true;  //Si no se pone en true entonces arranca undefined.
-    scope.guardar = "Guardar";
+    scope.nombreBtn = "Guardar";
+    scope.tituloNavbar = "Nueva publicacion";
+    scope.publicacionEtiquetas = [];
+
+    Etiquetas.getEtiquetas()
+        .success(function(response){
+            scope.tags = response;
+        });
 
     scope.onCancelar = function () {
         location.path("/");
     }
     scope.onGuardar = function(){
+        var etiquetas = new Object();
+        for (var i in scope.publicacionEtiquetas){
+            etiquetas[scope.publicacionEtiquetas[i].data.nombre] = true;
+        }
         var publicacion = {
             'titulo':scope.publicacionTitulo,
             'descripcion':scope.publicacionDescripcion,
@@ -18,13 +27,36 @@ app.controller("nuevaPublicacionController", ['$scope', '$location', 'Publicacio
                 '2': { 'nombre': 'OpenShift' }
             }};
         guardar(publicacion);
-        scope.guardar = "Guardando.."
+        scope.nombreBtn = "Guardando.."
     }
     scope.onDescripcion = function(){
         if(!scope.publicacionDescripcion.localeCompare("Inserte breve descripción.")) scope.publicacionDescripcion = null;
     }
     scope.onContenido = function(){
         if(!scope.publicacionContenido.localeCompare("Inserte contenido.")) scope.publicacionContenido = null;
+    }
+
+    scope.aniadirEtiqueta = function(){
+        for (var i in scope.tags) {
+            if(scope.tags[i].data.nombre == scope.etiquetaNombre) {
+                if(scope.publicacionEtiquetas.indexOf(scope.tags[i])==-1){
+                    scope.publicacionEtiquetas.push(scope.tags[i]);
+                    scope.etiquetaNombre = null;
+                    return;
+                }
+                else {
+                    alert("La etiqueta ya esta agregada");
+                    return;
+                }
+            }
+        }
+        alert("Inserte una etiqueta valida!");
+    }
+
+    scope.quitarEtiqueta = function(tag){
+        var index = scope.publicacionEtiquetas.indexOf(tag);
+        if(index != -1)scope.publicacionEtiquetas.splice(index, 1);
+        else alert("No funciono el indexOf");
     }
 
     var guardar = function(pub){
